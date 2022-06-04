ecb93cf63b94f58ab3074239defe2fe8159d5d8d
DominikPokora <dominik.pokora@chyronhego.com>
2022-06-04 19:29:32 +0200

    Added: feature file PageObjects class Steps class

---
+++ selenium-tutorial-step-by-step/PageObjects/LoginPage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selenium_tutorial_step_by_step.PageObjects
+{
+    internal class LoginPage
+    {
+    }
+}
